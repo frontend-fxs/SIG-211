94e2ad5a95df424061c64b2169cbfa4008c1fef8
frontend-fxs <oriol@fxstreet.com>
2018-12-24 15:13:15 +0100

    global useremail and platformname send emails

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,16 @@
+/* Global variables */
+var platformName = "";
+var userEmail = "";
+
 /* Platform selector functions */
 
-var resetHeaderBlueBackgroundStyles = function() {
+var resetHeaderBlueBackgroundStyles = function () {
     document.querySelector('#blueBackground').classList.remove('emailShowed');
     document.querySelector('#blueBackground').classList.remove('paymentShowed');
     document.querySelector('#blueBackground').classList.remove('thankyouShowed');
 };
 
-var resetAllActive = function(section) {
+var resetAllActive = function (section) {
 
     var MetatraderTriggerId = '#' + section + 'MetatraderTrigger';
     var NinjaTraderTriggerId = '#' + section + 'NinjaTraderTrigger';
@@ -22,26 +26,27 @@ var resetAllActive = function(section) {
 
 };
 
-var addActive = function(platformName, section) {
+var addActive = function (platformName, section) {
     resetAllActive(section);
     var sectionPlatformNameID = "#" + section + platformName + 'Trigger';
     var sectionPlatformNameObj = document.querySelector(sectionPlatformNameID);
     sectionPlatformNameObj.classList.add('active');
 };
 
-var showEmailLead = function(platformName, section) {
+var showEmailLead = function (platformNameP, section) {
     document.querySelector('#' + section + 'Payment').classList.add('hidden');
     document.querySelector('#' + section + 'ThankYou').classList.add('hidden');
     document.querySelector('#' + section + 'SendEmail').classList.remove('hidden');
-    document.querySelector('#' + section + 'Platform').value = platformName;
-    if (section == "header") {
+    document.querySelector('#' + section + 'Platform').value = platformNameP;
+    platformName = platformNameP;
+    if (section === "header") {
         resetHeaderBlueBackgroundStyles();
         document.querySelector('#blueBackground').classList.add('emailShowed');
     }
     addActive(platformName, section);
 };
 
-var showPaymentLink = function(section) {
+var showPaymentLink = function (section) {
     document.querySelector('#' + section + 'ThankYou').classList.add('hidden');
     document.querySelector('#' + section + 'SendEmail').classList.add('hidden');
     document.querySelector('#' + section + 'Payment').classList.remove('hidden');
@@ -52,36 +57,63 @@ var showPaymentLink = function(section) {
     addActive('Ctrader', section);
 };
 
-var thankYou = function(section) {
+var updateForm = function (Id) {
+    userEmail = document.querySelector('#' + Id).value;
+};
+
+var sendEmailLead = function () {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'https://externalservices-qa-bo-webapi-fxs.azurewebsites.net/api/Email/send');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            alert('Something went wrong. '  + xhr.responseText);
+        }
+        else if (xhr.status !== 200) {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send(JSON.stringify({
+        From:'signals@fxstreet.com',
+        To: userEmail,
+        Body:'',
+        TemplateName: platformName,
+        Subject:'Thanks for your interest in the FXstreet Market Impact Signals app'
+    }));
+};
+
+var thankYou = function (section) {
+    sendEmailLead();
     var thankyouId = '#' + section + 'ThankYou';
-    var formId = '#' + section + 'SendEmail';
+    var formId = '#' +section+'SendEmail';
     var thankyouObj = document.querySelector(thankyouId);
     var formObj = document.querySelector(formId);
     thankyouObj.classList.remove('hidden');
     formObj.classList.add('hidden');
-    document.onclick = function() {
+    document.onclick = function () {
         thankyouObj.classList.add('hidden');
     };
-    if (section == "header") {
+    if (section === "header") {
         resetHeaderBlueBackgroundStyles();
         document.querySelector('#blueBackground').classList.add('thankyouShowed');
-    };
+    }
 };
 
-var toggleHomeText = function() {
+var toggleHomeText = function () {
     document.querySelector('#platformHeader').classList.toggle('hidden');
     document.querySelector('#homeText').classList.toggle('opened');
 };
 
-var toggleText = function() {
+var toggleText = function () {
     document.querySelector('#platformText').classList.toggle('hidden');
     document.querySelector('#about').classList.toggle('opened');
     document.querySelector('#textCTA').classList.toggle('opened');
 };
 
-var togglePortfolio = function() {
+var togglePortfolio = function () {
     document.querySelector('#platformPortfolio').classList.toggle('hidden');
     document.querySelector('#portfolio').classList.toggle('opened');
     document.querySelector('#portfolioCTA').classList.toggle('opened');
 
-}
+};
